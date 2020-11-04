a473fb657836a5e254d6b355055cb287969d8d5a
Micky <miguel.angel.angulo72@gmail.com>
2020-11-04 12:45:01 -0400

    Added validation middlewares

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,15 +4,6 @@ const { validationResult } = require('express-validator');
 const createUser = (req, res = response) => {
     const { name, email, password } = req.body;
 
-    // Manage errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
-
     res.status(201).json({
         ok: true,
         msg: 'Register',
@@ -23,15 +14,7 @@ const createUser = (req, res = response) => {
 }
 
 const userLogin = (req, res = response) => {
-    const { email, password } = req.body;
-    // Manage errors
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        })
-    }
+    const { email, password } = req.body;    
     res.json({
         ok: true,
         msg: 'login'
--- routes/auth.js
+++ routes/auth.js
@@ -8,20 +8,24 @@ const { check } = require('express-validator');
 const router = Router();
 
 const { createUser, userLogin, revalidateToken } = require('../controllers/auth');
+const { fieldValidator } = require('../middlewares/field-validator');
+
 
 router.post(
     '/new',
     [//middlewares
         check('name', 'Name is required').not().isEmpty(),
         check('email', 'Email is required').isEmail(),
-        check('password', 'Password debe tener al menos 6 caracteres').isLength({ min: 6 })
+        check('password', 'Password debe tener al menos 6 caracteres').isLength({ min: 6 }),
+        fieldValidator
     ],
     createUser);
 
 router.post('/',
     [//middlewares        
         check('email', 'Email is required').isEmail(),
-        check('password', 'Password debe tener al menos 6 caracteres').isLength({ min: 6 })
+        check('password', 'Password debe tener al menos 6 caracteres').isLength({ min: 6 }),
+        fieldValidator
     ],
     userLogin);
 
