66a06dd0f82bca72e9daa10e7f78c1b0a5262059
Micky <miguel.angel.angulo72@gmail.com>
2020-11-04 19:44:07 -0400

    Added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 
 const { dbConnection } = require('./database/config');
 
@@ -8,6 +9,9 @@ const app = express();
 
 // Database
 dbConnection();
+
+// CORS
+app.use(cors());
 
 // Public
 app.use(express.static('public'));
