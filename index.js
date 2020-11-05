26049025fb8ac588f2d933e10316c5f9981ca27b
Micky <miguel.angel.angulo72@gmail.com>
2020-11-05 18:31:58 -0400

    Added event model

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(express.json());
 
 // Endpoints
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/events', require('./routes/events'));
 
 // listen requaries
 app.listen(process.env.PORT, () => {
